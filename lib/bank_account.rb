6dc87a6400ebfc079dd72c32cc7a7ae0e6571242
Lillian Zhang <lilyzhangyun@gmail.com>
2018-06-29 10:53:49 +0000

    automatically backed up by learn

--- lib/bank_account.rb
+++ lib/bank_account.rb
@@ -1,63 +1,35 @@
 require 'pry'
 
-class Transfer
-  # your code here
-  attr_accessor :sender, :receiver, :status, :amount, :count
+class BankAccount
 
+    attr_reader :name
+    attr_accessor :balance, :status
 
-  @@transactions = []
-  
-  def initialize(sender, receiver, amount)
-      @sender = sender
-      @receiver = receiver
-      @status = "pending"
-      @amount = amount
-      @count = 0
+    def initialize(name)
+        @name = name
+        @balance = 1000
+        @status = "open"
+    end
 
-  end
+    def deposit(amount)
+        self.balance += amount
+    end
 
-  def valid? 
-      if sender.valid? && receiver.valid?
-          true 
-      else 
-          false
-      end
-  end 
-    
-  def execute_transaction
-      if self.count == 0 && sender.valid? == true && sender.balance > self.amount
-           # binding.pry
-          receiver.deposit(self.amount) 
-          sender.deposit(-self.amount)
-          self.status = "complete"
-          self.count += 1
-          @@transactions << self.object_id
-      else
-          self.status = "rejected"
-          return "Transaction rejected. Please check your account balance."
+    def display_balance
+        "Your balance is $#{self.balance}."
+    end
 
-      end
-  end 
-
-  def self.get_all_transactions
-      @@transactions
-  end
-
-  def reverse_transfer
-      @@transactions.each do |x|
-          if self.object_id == x
-              receiver.deposit(-self.amount) 
-            sender.deposit(self.amount)
-            self.status = "reversed"
-          end
+    def valid?
+        if self.status == 'open' && self.balance > 0
+            return true
+        else
+            return false
         end
     end
 
- 
- end
+    def close_account
+        self.status = "closed"
+    end
+end
 
-# transfer = Transfer.new(sean, lily, 40)
-# transfer2 = Transfer.new(lily, sean, 75)
-
-# transfer.execute_transaction
-# transfer2.execute_transaction
+bank1 = BankAccount.new("Lillian's Bank")
